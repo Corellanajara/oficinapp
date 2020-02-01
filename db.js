48500ea48d20d336a1b32bb8bacd850c58009276
Cristopher Orellana <cristopherorellana@Cristophers-MacBook-Pro.local>
2020-02-01 02:29:42 -0300

    cambios insersion

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'clavesistema',
+  password: '78912300aA',
   database: 'restful_db'
 });
 //conectar a la base de datos
