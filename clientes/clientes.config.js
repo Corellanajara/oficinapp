a9128baa1f8998d957d7fd61b5d94bf960cb66bf
Cristopher Orellana <cristopherorellana@Cristophers-MacBook-Pro.local>
2020-02-05 14:44:23 -0300

    changes in clientes

--- clientes/clientes.config.js
+++ clientes/clientes.config.js
@@ -20,7 +20,7 @@ exports.routesConfig = function (app) {
 
   //Agregar uno
   app.post('/api/clientes',(req, res) => {
-    let data = {nombre: req.body.nombre, giro: req.body.giro,rut:req.body.rut,direccion:req.body.direccion,comuna:req.body.comuna,ciudad:req.body.ciudad,contacto:req.body.contacto,tipoCompra:req.body.tipoCompra};
+    let data = {estado:true,nombre: req.body.nombre, giro: req.body.giro,rut:req.body.rut,direccion:req.body.direccion,comuna:req.body.comuna,ciudad:req.body.ciudad,contacto:req.body.contacto,tipoCompra:req.body.tipoCompra};
     let sql = "INSERT INTO cliente SET ?";
     console.log(data);
     console.log(req);
@@ -32,7 +32,7 @@ exports.routesConfig = function (app) {
 
   //Actualizar gasto
   app.put('/api/clientes/:id',(req, res) => {
-    let sql = "UPDATE cliente SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.params.id;
+    let sql = "UPDATE cliente SET estado = '"+req.body.estado+"', nombre='"+req.body.nombre+"', giro='"+req.body.giro+"', rut='"+req.body.rut+"', direccion='"+req.body.direccion+"', ciudad='"+req.body.ciudad+"', contacto='"+req.body.contacto+"' WHERE product_id="+req.params.id;
     let query = conn.query(sql, (err, results) => {
       if(err) throw err;
       res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
