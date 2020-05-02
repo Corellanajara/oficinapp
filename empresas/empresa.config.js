d63a279d53976586f72e665805582e7f6dc8e9d1
corellanajara <corellanajara@hotmail.com>
2020-05-02 18:50:24 +0000

    generando cotiazciones

--- empresas/empresa.config.js
+++ empresas/empresa.config.js
@@ -21,7 +21,7 @@ exports.routesConfig = function (app) {
 
   //Agregar uno
   app.post('/api/empresas',(req, res) => {
-    let data = {estado:true,nombre: req.body.nombre, giro: req.body.giro,rut:req.body.rut,direccion:req.body.direccion,comuna:req.body.comuna,ciudad:req.body.ciudad,contacto:req.body.contacto};
+    let data = {estado:true,nombre: req.body.nombre, giro: req.body.giro,rut:req.body.rut,direccion:req.body.direccion,comuna:req.body.comuna,ciudad:req.body.ciudad,contacto:req.body.contacto,url:req.body};
     let sql = "INSERT INTO empresa SET ?";
     console.log(data);
     console.log(req);
@@ -33,7 +33,7 @@ exports.routesConfig = function (app) {
 
   //Actualizar gasto
   app.put('/api/empresas/:id',(req, res) => {
-    let sql = "UPDATE empresa SET estado = '"+req.body.estado+"', nombre='"+req.body.nombre+"', giro='"+req.body.giro+"', rut='"+req.body.rut+"', direccion='"+req.body.direccion+"', ciudad='"+req.body.ciudad+"', contacto='"+req.body.contacto+"' WHERE id="+req.params.id;
+    let sql = "UPDATE empresa SET estado = '"+req.body.estado+"', nombre='"+req.body.nombre+"', giro='"+req.body.giro+"', rut='"+req.body.rut+"', direccion='"+req.body.direccion+"', ciudad='"+req.body.ciudad+"',url = '"+req.body.url+"',comuna = '"+req.body.comuna+"', contacto='"+req.body.contacto+"' WHERE id="+req.params.id;
     let query = conn.query(sql, (err, results) => {
       if(err) throw err;
       res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
